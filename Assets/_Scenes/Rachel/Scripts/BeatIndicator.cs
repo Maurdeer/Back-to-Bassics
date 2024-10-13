30c78699e5023a3c17825d50cfe34f487e915da2
Rachel Kim <rachelgekim@gmail.com>
2024-10-13 00:05:58 -0400

    remove vignette implementation and add converging bar

--- Assets/_Scenes/Rachel/Scripts/BeatIndicator.cs
+++ Assets/_Scenes/Rachel/Scripts/BeatIndicator.cs
@@ -1,66 +1,44 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class BeatIndicator : Conductable
 {
-    [Header("Vignette")]
-    [SerializeField] private bool isVignette;
-    [SerializeField] private Volume vignetteVolume;
-
-    private VolumeProfile vignetteProfile;
-    private UnityEngine.Rendering.Universal.Vignette vignette;
-
     [Header("Indicator")]
-    [SerializeField] private Sprite beatSprite;
-
-    private Image image;
-
-    private void Awake()
-    {
-        image = GetComponent<Image>();
-
-        vignetteProfile = vignetteVolume.sharedProfile;
-        vignetteVolume.profile.TryGet(out vignette);
-    }
+    [SerializeField] private Slider rightSlider;
+    [SerializeField] private Slider leftSlider;
 
     private void Start()
     {
         Enable();
     }
 
-    protected override void OnFullBeat()
-    {
-        if (isVignette)
-        {
-            StartCoroutine(IVignette());
-        }
-        StartCoroutine(IBeat());
-    }
-
     private IEnumerator IBeat()
     {
-        Sprite prevSprite = image.sprite;
+        float elapsedTime = 0f;
 
-        image.sprite = beatSprite;
+        float rightStartValue = 1f;
+        float leftStartValue = 1f;
 
-        yield return new WaitForSeconds(0.1f);
+        float targetValue = 0.5f;
 
-        image.sprite = prevSprite;
+        while (elapsedTime < Conductor.Instance.spb)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / Conductor.Instance.spb;
+
+            rightSlider.value = Mathf.Lerp(rightStartValue, targetValue, t);
+            leftSlider.value = Mathf.Lerp(leftStartValue, targetValue, t);
+
+            yield return null;
+        }
+
+        rightSlider.value = 1f;
+        leftSlider.value = 1f;
     }
 
-    private IEnumerator IVignette()
+    protected override void OnFullBeat()
     {
-        if (vignette)
-        {
-            float originalIntensity = vignette.intensity.value;
-
-            vignette.intensity.value += 0.2f;
-
-            yield return new WaitForSeconds(0.1f);
-
-            vignette.intensity.value = originalIntensity;
-        }
+        StartCoroutine(IBeat());
     }
 }
