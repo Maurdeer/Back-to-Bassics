375bad3d78999a8e08dfd2e8a7404b3d72371330
Rachel Kim <rachelgekim@gmail.com>
2024-10-21 19:59:27 -0400

    add onDestroy check to conductables

--- Assets/_Scenes/Rachel/Scripts/BeatIndicator.cs
+++ Assets/_Scenes/Rachel/Scripts/BeatIndicator.cs
@@ -1,16 +1,5 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class BeatIndicator : Conductable
+public class BeatIndicator : MonoBehaviour
 {
-    [SerializeField] private IndicatorLineSpawner leftSpawner;
-    [SerializeField] private IndicatorLineSpawner rightSpawner;
-
-    private void Start()
-    {
-        Enable();
-
-        leftSpawner.Enable();
-        rightSpawner.Enable();
-    }
 }
--- Assets/_Scenes/Rachel/Scripts/LineSpawner.cs
+++ Assets/_Scenes/Rachel/Scripts/LineSpawner.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class IndicatorLineSpawner : Conductable
 {
     [SerializeField] private GameObject line;
+
+    private void Start()
+    {
+        Enable();
+    }
 
     protected override void OnFullBeat()
     {
